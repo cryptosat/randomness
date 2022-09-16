ade3b11999e6ff0f02812be9fd241037927d1049
Yan Michalevsky <yan@anjuna.io>
2022-09-15 17:56:07 -0700

    Updated Random Beacon documentation

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Cryptosat Randomness Beacon',
-  tagline: 'on Velas Blockchain',
+  title: 'Cryptosat Random Beacon',
+  tagline: 'for Velas Blockchain',
   url: 'https://cryptosat.github.io/',
   baseUrl: '/randomness/',
   onBrokenLinks: 'throw',
--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -4,32 +4,34 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
+    title: 'Verifiable',
+    Svg: require('@site/static/img/verifiable.svg').default,
+    description: (
+      <>
+        Cryptosat provides a Verifiable Random Beacon. Users can directly verify that 
+        randomness is originated by one of our satellites by checking the digital signature
+        attached to the beacon.
+      </>
+    ),
+  },
+  {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/easy-to-use.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Cryptosat offers a simple HTTP API, as well an a Solidty SDK that enables
+        to use the random beacon in Web2.0 and Web3.0 applications alike.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Secure and Unbiased',
+    Svg: require('@site/static/img/secure.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Our Random Beacon is generated from true entropy collected in space, where
+        no physical access to our satellites is possible, preventing the strongest adversaries
+        from tampering with it.
       </>
     ),
   },
